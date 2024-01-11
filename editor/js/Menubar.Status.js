53ccab5111e7f64e54b62a987121921be16e8a11
charles Z <charles.zecevic@ls-group.fr>
2024-01-11 17:23:30 +0100

    change place of fullscreen

--- editor/js/Menubar.Status.js
+++ editor/js/Menubar.Status.js
@@ -10,64 +10,6 @@ function MenubarStatus(editor) {
 	const container = new UIPanel();
 	container.setClass("menu right");
 
-	// -------------------------------------------------- fullscreen --------------------------------------------------
-
-	const displayOutliner = new UIButton(
-		strings.getKey("menubar/status/outlinerDisplay")
-	);
-	displayOutliner.setMarginRight("10px");
-	document.getElementById("OutlinePanel").style.display = "block";
-	displayOutliner.onClick(() => {
-		// const
-		const outliner = document.getElementById("OutlinePanel").style;
-		const toolbar = document.getElementById("toolbar").style;
-		const info = document.getElementById("info").style;
-		const viewport = document.getElementById("viewport").style;
-		const sidebar = document.getElementById("sidebar").style;
-
-		if (outliner.display === "block") {
-			outliner.display = "none";
-			sidebar.display = "none";
-			toolbar.left = "25px";
-			info.left = "25px";
-			viewport.left = "0px";
-			viewport.right = "0px";
-		} else {
-			outliner.display = "block";
-			sidebar.display = "block";
-			toolbar.left = outliner.width
-				? parseInt(outliner.width, 10) + 25 + "px"
-				: parseInt(toolbar.left, 10) + 200 + "px";
-			info.left =
-				outliner.width > 200 ? "25px" : parseInt(toolbar.left, 10) - 200 + "px";
-			console.log(parseInt(outliner.width, 10) + "px");
-			viewport.left = outliner.width
-				? parseInt(outliner.width, 10) + "px"
-				: "200px";
-			viewport.right = sidebar.width
-				? parseInt(sidebar.width, 10) + "px"
-				: "300px";
-		}
-		signals.windowResize.dispatch();
-	});
-	container.add(displayOutliner);
-
-	// -------------------------------------------------- save --------------------------------------------------
-
-	const save = new UIButton(strings.getKey("menubar/status/save"));
-	save.onClick(() => {
-		editor.signals.sceneGraphChanged.dispatch();
-		editor.signals.savingStarted.dispatch();
-		editor.storage.set(editor.toJSON());
-		editor.signals.savingFinished.dispatch();
-	});
-	container.add(save);
-
-	const version = new UIText("r" + THREE.REVISION);
-	version.setClass("title");
-	version.setOpacity(0.5);
-	container.add(version);
-
 	// -------------------------------------------------- autosave --------------------------------------------------
 
 	const autosave = new UIBoolean(
@@ -95,6 +37,22 @@ function MenubarStatus(editor) {
 	});
 
 	container.add(autosave);
+
+	// -------------------------------------------------- save --------------------------------------------------
+
+	const save = new UIButton(strings.getKey("menubar/status/save"));
+	save.onClick(() => {
+		editor.signals.sceneGraphChanged.dispatch();
+		editor.signals.savingStarted.dispatch();
+		editor.storage.set(editor.toJSON());
+		editor.signals.savingFinished.dispatch();
+	});
+	container.add(save);
+
+	const version = new UIText("r" + THREE.REVISION);
+	version.setClass("title");
+	version.setOpacity(0.5);
+	container.add(version);
 
 	return container;
 }
--- editor/js/Menubar.View.js
+++ editor/js/Menubar.View.js
@@ -1,80 +1,78 @@
-import { UIPanel, UIRow } from './libs/ui.js';
+import { UIPanel, UIRow } from "./libs/ui.js";
 
-function MenubarView( editor ) {
-
+function MenubarView(editor) {
 	const strings = editor.strings;
+	const signals = editor.signals;
 
 	const container = new UIPanel();
-	container.setClass( 'menu' );
+	container.setClass("menu");
 
 	const title = new UIPanel();
-	title.setClass( 'title' );
-	title.setTextContent( strings.getKey( 'menubar/view' ) );
-	container.add( title );
+	title.setClass("title");
+	title.setTextContent(strings.getKey("menubar/view"));
+	container.add(title);
 
 	const options = new UIPanel();
-	options.setClass( 'options' );
-	container.add( options );
+	options.setClass("options");
+	container.add(options);
 
 	// Fullscreen
 
 	const option = new UIRow();
-	option.setClass( 'option' );
-	option.setTextContent( strings.getKey( 'menubar/view/fullscreen' ) );
-	option.onClick( function () {
+	option.setClass("option");
+	option.setTextContent(strings.getKey("menubar/view/fullscreen"));
+	document.getElementById("OutlinePanel").style.display = "block";
+	option.onClick(function () {
+		const outliner = document.getElementById("OutlinePanel").style;
+		const toolbar = document.getElementById("toolbar").style;
+		const info = document.getElementById("info").style;
+		const viewport = document.getElementById("viewport").style;
+		const sidebar = document.getElementById("sidebar").style;
 
-		if ( document.fullscreenElement === null ) {
-
-			document.documentElement.requestFullscreen();
-
-		} else if ( document.exitFullscreen ) {
-
-			document.exitFullscreen();
-
+		if (outliner.display === "block") {
+			outliner.display = "none";
+			sidebar.display = "none";
+			toolbar.left = "25px";
+			info.left = "25px";
+			viewport.left = "0px";
+			viewport.right = "0px";
+		} else {
+			outliner.display = "block";
+			sidebar.display = "block";
+			toolbar.left = outliner.width
+				? parseInt(outliner.width, 10) + 25 + "px"
+				: parseInt(toolbar.left, 10) + 200 + "px";
+			info.left =
+				outliner.width > 200 ? "25px" : parseInt(toolbar.left, 10) - 200 + "px";
+			console.log(parseInt(outliner.width, 10) + "px");
+			viewport.left = outliner.width
+				? parseInt(outliner.width, 10) + "px"
+				: "200px";
+			viewport.right = sidebar.width
+				? parseInt(sidebar.width, 10) + "px"
+				: "300px";
 		}
-
-		// Safari
-
-		if ( document.webkitFullscreenElement === null ) {
-
-			document.documentElement.webkitRequestFullscreen();
-
-		} else if ( document.webkitExitFullscreen ) {
-
-			document.webkitExitFullscreen();
-
-		}
-
-	} );
-	options.add( option );
+		signals.windowResize.dispatch();
+	});
+	options.add(option);
 
 	// VR (Work in progress)
 
-	if ( 'xr' in navigator ) {
-
-		navigator.xr.isSessionSupported( 'immersive-vr' )
-			.then( function ( supported ) {
-
-				if ( supported ) {
-
-					const option = new UIRow();
-					option.setClass( 'option' );
-					option.setTextContent( 'VR' );
-					option.onClick( function () {
-
-						editor.signals.toggleVR.dispatch();
-
-					} );
-					options.add( option );
-
-				}
-
-			} );
-
+	if ("xr" in navigator) {
+		navigator.xr.isSessionSupported("immersive-vr").then(function (supported) {
+			if (supported) {
+				const option = new UIRow();
+				option.setClass("option");
+				option.setTextContent("VR");
+				option.onClick(function () {
+					editor.signals.toggleVR.dispatch();
+				});
+				options.add(option);
+			}
+		});
 	}
 
 	return container;
-
 }
 
 export { MenubarView };
